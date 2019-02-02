acb73cf2a9c80bcc9130b779ca8a5822b68af0d7
Christopher Goel <christopher.goel@gmail.com>
2019-02-02 03:35:42 -0500

    Define new Android Manifest

--- app/src/main/java/com/example/bennet/commute/LoginActivity.java
+++ app/src/main/java/com/example/bennet/commute/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         // Set up the login form.
 
-        email = findViewById(R.id.emailSign);
+        email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         btn = findViewById(R.id.email_sign_in_button);
 
