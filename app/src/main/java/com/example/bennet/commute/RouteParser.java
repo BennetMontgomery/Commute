75d1950bb349f9b785f38781327d6c9eaedfca19
Bennet Montgomery <bennetlogan@gmail.com>
2019-02-02 03:45:41 -0500

    Updated RouteParser to give correct url for api query

--- app/src/main/java/com/example/bennet/commute/RouteParser.java
+++ app/src/main/java/com/example/bennet/commute/RouteParser.java
@@ -5,20 +5,36 @@ public class RouteParser {
     private String destination;
 
     public boolean parse(String val, boolean isOrigin) {
-        val.replaceAll("[\\p{Punct}][\\p{Blank}]", " ");
-        val.replaceAll("[\\p{Punct}]", " ");
+        val = val.replaceAll("[\\p{Punct}][\\p{Blank}]", " ");
+        val = val.replaceAll("[\\p{Punct}]", " ");
 
         if(val.matches("[\\p{Blank}]*[1-9]*([\\p{Blank}]*[a-zA-Z]*)*")) {
+            StringBuilder value = new StringBuilder(val);
+
             if(isOrigin) {
-                this.origin = val;
+                this.origin = this.formatForUrl(val);
             } else {
-                this.destination = val;
+                this.destination = this.formatForUrl(val);
             }
 
             return true;
         } else {
             return false;
         }
+    }
+
+    private String formatForUrl(String toFormat) {
+        while(toFormat.charAt(0) == ' ') {
+            toFormat = toFormat.substring(1);
+        }
+
+        while(toFormat.charAt(toFormat.length() - 1) == ' ') {
+            toFormat = toFormat.substring(0, toFormat.length() - 1);
+        }
+
+        toFormat = toFormat.replaceAll("[\\p{Blank}]", "+");
+
+        return toFormat;
     }
 
     public String getUrlPrefix() {
