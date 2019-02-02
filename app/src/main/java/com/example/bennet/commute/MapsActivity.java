058218ee2063620236d5a2dcd4f7e578accd7e65
Christopher Goel <christopher.goel@gmail.com>
2019-02-02 16:11:07 -0500

    Add HTTP JSON support

--- app/src/main/java/com/example/bennet/commute/MapsActivity.java
+++ app/src/main/java/com/example/bennet/commute/MapsActivity.java
@@ -21,6 +21,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -28,6 +37,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private FloatingActionButton mFAB;
     private FusedLocationProviderClient mFusedLocationClient;
     private final int REQUEST_LOCATION = 4;
+    private JSONObject mapData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +49,48 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mFAB.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 System.out.println("End my suffering");
+                URL url;
+                StringBuffer response = new StringBuffer();
+                try {
+                    url = new URL("https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&alternatives=true&key=AIzaSyCTXdNtnh6_yKnLLwHo_efKxOvRLWzxg0k");
+                    System.out.println("URL: " + url.toString());
+                } catch (MalformedURLException e) {
+                    throw new IllegalArgumentException("invalid url");
+                }
+
+                HttpURLConnection conn = null;
+                try {
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoOutput(false);
+                    conn.setDoInput(true);
+                    conn.setUseCaches(false);
+                    conn.setRequestMethod("GET");
+                    conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+
+                    // handle the response
+                    int status = conn.getResponseCode();
+                    if (status != 200) {
+                        throw new IOException("Post failed with error code " + status);
+                    } else {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                        String inputLine;
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
+                        }
+                        in.close();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (conn != null) {
+                        conn.disconnect();
+                    }
+
+                    //Here is your json in string format
+                    String responseJSON = response.toString();
+                    System.out.println("responseJSON: " + responseJSON);
+                }
+
             }
         });
         System.out.println("email (after intent): " + name);
