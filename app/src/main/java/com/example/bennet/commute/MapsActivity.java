a0253c3bb15d1a1971d1a4d7422ffcb5b8b3d05e
Bennet Montgomery <bennetlogan@gmail.com>
2019-02-03 04:06:32 -0500

    Added arrays of paths and travel times

--- app/src/main/java/com/example/bennet/commute/MapsActivity.java
+++ app/src/main/java/com/example/bennet/commute/MapsActivity.java
@@ -21,6 +21,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
@@ -62,6 +64,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private JSONObject mapData;
     private Button goButton;
     private ProgressDialog progress;
+    private String[] rawPaths;
+    private int[] travelTimes;
 
     EditText origin;
     EditText finaldest;
@@ -128,12 +132,36 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 List<LatLng> polyLine = PolyUtil.decode("e`miGhmocN{OiAsAiLeDwh@qLmn@oJir@sC}RyGpCqd@xJsXlSeq@dKgb@gIa]cMcc@}r@I_x@oU{g@_a@mSeb@}b@kQ_AmXzScoBxTwvBbR{u@tHiL}NeC}bBkP}hBipA{xJux@wyCeg@uvGtYqbCkVos@mjBgiDodA_{ByaA{lCit@o{Bym@evEq`AgoGyMscBdBqcB`CkhCocA}yGmu@waFkCixBno@ycBxQc}@kOkvCol@keJmz@qnCy]uaCoY{wHevCusUicFg`_@}c@}_JijAeqHbImpAnMqzAmd@ugDim@{fBqLasBsx@omCB}lDvHacAlSql@zCqcA_nAagLsp@mkKg]k_Deh@mpDkf@e`Buf@mgByReeAkm@wv@q{@i_Gy|@miAkQki@gLioBsfAuaFyp@arAkf@aoB{wAotFkDmlDet@mkBovAu}D}vBe}FceBymKirBuiM}z@q|C}eAcbHaa@goCg@uz@zYmgBsJk|BcmBa}WiaBuiU_a@o{Byq@epCtBshAD_s@iX{yAiPgrAiC_}ByTsqGmWgtQuYsrOwNweLaO_vBzaAqoDvXwjCt_@cdCrCcaBiWuw@_l@waC_c@kvCi_AqlCmi@ki@ynA{nDmk@svHmUm}GwYoiGgGgzAs]u{Akh@aqFuHogBtJ_bARkrAoc@}iCc{@_aDqlBmrGcL_pCr@ynBwWmmBuQspAuZmu@ox@w|A_b@isAs_AmsAojB_uCanBqh@gjBcrAucBouAk~BchCiiAwjAa_@siCxDquBql@abB}U}RymAbCog@oNuoB{{CahAog@}]i^mpAkwCqcEq`HunAabByeB_jCybIo`K_^sq@_Y_eB_cActB_eDouFsn@}|@{q@s]od@kYy`@ip@ebEivHctAggCguEu`Jg{CcqHmbDgzKgcE}tMa}BemH}oAo^c~DwjMuhDciL_lCuaKuy@mjFxu@cjDf`@ajB`HwtBiBulFwr@_wC{L_p@kCojCsLojC}Y}dBc}BosHwmBwzEabEsyKah@yn@_h@_yAcz@itC{bBq~Fuh@_mBqU{QmXaRke@{lAabB}~Dmv@m|BecAiiDovEk}Lm_E}fFoe@_aA_tAkgEibDikKys@e|Byq@ogA}mAsfBqr@g_B_Sgj@eDucBsk@sxC_aA_~@sYu_AshB{{Hs`@qrEefAa{Gui@y{CvJ_vGwF}|@fBw|DrDmcE`]cgBqLenAqv@i}AqiBo}D}a@kP{q@gbAmUw]eOgOkNdUip@gr@aKqRxDwJ");
                 mMap.addPolyline(new PolylineOptions().addAll(polyLine));
                 System.out.println("Time test");
-                String myUrl = "https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&alternatives=true&key=AIzaSyCTXdNtnh6_yKnLLwHo_efKxOvRLWzxg0k";
+                String myUrl = "https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&departure_time=now&alternatives=true&key=AIzaSyCTXdNtnh6_yKnLLwHo_efKxOvRLWzxg0k";
                 String result;
                 HttpGetRequest getRequest = new HttpGetRequest();
                 try {
                     result = getRequest.execute(myUrl).get();
-                    System.out.println("result: " + result);
+                    result = result.replaceAll("\\p{Blank}", "");
+                    result = result.substring(result.indexOf("routes"), result.indexOf("\"status\""));
+                    int num = count(result, "overview_polyline");
+                    rawPaths = new String[num];
+                    for(int i = 0; i < num; i++) {
+                        String path = result;
+                        for(int j = 0; j <= i; j++) {
+                            path = path.substring(path.indexOf("overview_polyline") + 1);
+                        }
+
+                        path = path.substring(path.indexOf("points") + 9, path.indexOf("},") - 1);
+                        rawPaths[i] = path;
+                    }
+
+                    travelTimes = new int[num];
+                    for(int i = 0; i < num; i++) {
+                        String trafficVal = result;
+                        for(int j = 0; j <= i; j++) {
+                            trafficVal = trafficVal.substring(trafficVal.indexOf("duration_in_traffic") + 1);
+                        }
+
+                        trafficVal = trafficVal.substring(trafficVal.indexOf("in_traffic") + 11, trafficVal.indexOf("end_address"));
+                        trafficVal = trafficVal.substring(trafficVal.indexOf("value") + 7, trafficVal.indexOf("},"));
+                        travelTimes[i] = Integer.parseInt(trafficVal);
+                    }
                 } catch (ExecutionException e) {
                     System.out.println("e: " + e);
                 } catch (InterruptedException e) {
@@ -305,5 +333,20 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     });
         }
     }
+
+    public static int count(final String string, final String substring)
+    {
+        int count = 0;
+        int idx = 0;
+
+        while ((idx = string.indexOf(substring, idx)) != -1)
+        {
+            idx++;
+            count++;
+        }
+
+        return count;
+    }
+
 
 }
